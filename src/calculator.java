a7fb66aa82886ba18168c9928e249bc79a81c63e
jagatthak <jagatthakkar25@gmail.com>
2022-04-30 16:21:51 +0100

    Add added

--- src/calculator.java
+++ src/calculator.java
@@ -1,6 +1,14 @@
 public class calculator {
+
+    public static void add(int a,int b){
+        System.out.println(a+b);}
+
     public static void main(String[] args) {
+
         System.out.println("hello world");
+        add(10,20);
+
+        }
     }
 
-}
+
